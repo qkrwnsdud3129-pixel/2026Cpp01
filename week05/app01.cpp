15c55532f0b8545f46664f627085ea7510006d84
박준영 <qkrwnsdud3129@gmail.com>
2026-04-02 12:44:11 +0900

    week05: user-defined types && memory management v0.4

---
+++ week05/app01.cpp
@@ -0,0 +1,34 @@
+// heap memory : dtnamic memory 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//int humans = 100;
+	//int ages[humans];
+
+	int age;
+	//int* a = &age; 포인터 변수는 메모리 번지 주소만을 사용할수있다
+
+	int humans = 0;
+	cout << "몇 명?";
+	cin >> humans;
+	int* ages = new int[humans]; // heap memory
+
+	for (int i = 0; i < humans; i++) {
+		cout << "나이? ";
+		cin >> age;
+		*(ages + i) = age;
+	}
+	for (int i = 0; i < humans; i++) {
+		cout << *(ages + i) << '\n';
+	}
+	//cout << ages << '\n';
+	//cout << &humans << '\n';
+	//cout << &age << '\n';
+
+	delete[] ages; // free memory
+
+	return 0;
+}
