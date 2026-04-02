ce23482263beb316ba1fab9f6e65ce6a47baa39a
박준영 <qkrwnsdud3129@gmail.com>
2026-04-02 13:21:52 +0900

    week05: user-defined types && memory management v0.5

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -1,34 +1,47 @@
-// heap memory : dtnamic memory 
-
+// heap memory : dynamic memory allocation
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	//int humans = 100;
-	//int ages[humans];
+    //int humans = 100;
+    //int ages[humans];
 
-	int age;
-	//int* a = &age; 포인터 변수는 메모리 번지 주소만을 사용할수있다
+    int age;
+    //int* a = &age;  // 포인터 변수는 메모리 번지 주소만을 받는다.
 
-	int humans = 0;
-	cout << "몇 명?";
-	cin >> humans;
-	int* ages = new int[humans]; // heap memory
+    int age, totalPrice = 0;
+    int humans = 0;
+    cout << "몇 명? ";
+    cin >> humans;
 
-	for (int i = 0; i < humans; i++) {
-		cout << "나이? ";
-		cin >> age;
-		*(ages + i) = age;
-	}
-	for (int i = 0; i < humans; i++) {
-		cout << *(ages + i) << '\n';
-	}
-	//cout << ages << '\n';
-	//cout << &humans << '\n';
-	//cout << &age << '\n';
+    int* ages = new int[humans];
 
-	delete[] ages; // free memory
+    for (int i = 0; i < humans; i++) {
+        cin >> age;
+        *(ages + i) = age;
+    }
 
-	return 0;
+    int kid = 5000, adult = 10000, senior = 7000;
+
+    for (int i = 0; i < humans; i++) {
+        cout << *(ages + i) << '\n';
+        //cout << *(ages + i) << '\n';
+        age = *(ages + i);
+        if (age >= 65)
+            totalPrice = totalPrice + senior;
+        else if (age >= 19)
+            totalPrice = totalPrice + adult;
+        else
+            totalPrice = totalPrice + kid;
+    }
+
+    //cout << ages << '\n';
+    //cout << &humans << '\n';
+    //cout << &age << '\n';
+
+    cout << "총 요금 : " << totalPrice << '\n';
+    delete[] ages;  // free memory
+
+    return 0;
 }
