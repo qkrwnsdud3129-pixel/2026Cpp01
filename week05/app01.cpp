cec2236676262203f700ca2b183299537a3d41db
박준영 <qkrwnsdud3129@gmail.com>
2026-04-02 13:24:56 +0900

    week05: user-defined types && memory management v0.6

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -4,30 +4,21 @@ using namespace std;
 
 int main()
 {
-    //int humans = 100;
-    //int ages[humans];
-
-    int age;
-    //int* a = &age;  // 포인터 변수는 메모리 번지 주소만을 받는다.
-
     int age, totalPrice = 0;
     int humans = 0;
     cout << "몇 명? ";
     cin >> humans;
-
-    int* ages = new int[humans];
+    int* ages = new int[humans];  // heap memory
 
     for (int i = 0; i < humans; i++) {
+        cout << "나이? ";
         cin >> age;
         *(ages + i) = age;
     }
-
     int kid = 5000, adult = 10000, senior = 7000;
 
     for (int i = 0; i < humans; i++) {
-        cout << *(ages + i) << '\n';
-        //cout << *(ages + i) << '\n';
-        age = *(ages + i);
+        age = ages[i];  // 포인터는 배열처럼 배열은 포인터처럼 사용 가능
         if (age >= 65)
             totalPrice = totalPrice + senior;
         else if (age >= 19)
@@ -35,13 +26,7 @@ int main()
         else
             totalPrice = totalPrice + kid;
     }
-
-    //cout << ages << '\n';
-    //cout << &humans << '\n';
-    //cout << &age << '\n';
-
     cout << "총 요금 : " << totalPrice << '\n';
     delete[] ages;  // free memory
 
     return 0;
-}
