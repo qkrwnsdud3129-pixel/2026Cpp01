8fc961c94e97b457bcf547742c0d3471a447ad67
박준영 <qkrwnsdud3129@gmail.com>
2026-05-28 11:29:23 +0900

    week13: inheritance v3.5

--- week13/app01/app01/pokemon.h
+++ week13/app01/app01/pokemon.h
@@ -1,1 +1,13 @@
-#pragma once 
+#pragma once
+
+class Pokemon
+{
+private:
+    int hp;
+public:
+    Pokemon();
+    Pokemon(int hp);
+    Pokemon(const Pokemon& pokemon);
+    void set(int hp);
+    int getHp() const;
+};
