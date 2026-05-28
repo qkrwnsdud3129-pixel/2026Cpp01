1ea45d1e76ad9c4830564663ae860d039115bc2a
박준영 <qkrwnsdud3129@gmail.com>
2026-05-28 11:30:51 +0900

    week13: inheritance v3.6

--- week13/app01/app01/pikachu.h
+++ week13/app01/app01/pikachu.h
@@ -1,2 +1,15 @@
 #pragma once
- 
+#include "pokemon.h"
+
+class Pikachu : public Pokemon  // is-a
+{
+private:
+    int electricAttack;
+public:
+    Pikachu();
+    Pikachu(int hp, int electricAttack);
+    Pikachu(const Pikachu& pikachu);
+    void set(int hp);
+    void set(int hp, int electricAttack);
+    int getElectricAttack() const;
+};
